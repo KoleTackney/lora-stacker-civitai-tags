052e93f5cdb3cd7a8d14f46ef1e0524e3aeaf4ef
KoleT <kole.tackney@outlool.com>
2026-03-27 12:38:45 -0400

    Bump version to 1.0.1 in `__init__.py`.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .src.my_custom_nodepack.nodes import NODE_DISPLAY_NAME_MAPPINGS
 
 __author__ = "Lorastack Tag autoloader"
 __email__ = "kole.tackney@gmail.com"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'CC_VERSION']
 
