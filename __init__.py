220a86dbb37ba595c8c165b278e4e854e3f3d9bf
KoleT <kole.tackney@outlool.com>
2026-03-27 12:37:06 -0400

    Update author email and refactor `__all__` in `__init__.py`.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,10 @@
 """Top-level package for lora-stacker-civitai-tags."""
-
-__all__ = [
-    "NODE_CLASS_MAPPINGS",
-    "NODE_DISPLAY_NAME_MAPPINGS",
-
-]
+from .src.my_custom_nodepack.nodes import NODE_CLASS_MAPPINGS
+from .src.my_custom_nodepack.nodes import NODE_DISPLAY_NAME_MAPPINGS
 
 __author__ = "Lorastack Tag autoloader"
 __email__ = "kole.tackney@gmail.com"
 __version__ = "1.0.0"
 
-from .src.my_custom_nodepack.nodes import NODE_CLASS_MAPPINGS
-from .src.my_custom_nodepack.nodes import NODE_DISPLAY_NAME_MAPPINGS
-
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'CC_VERSION']
 
